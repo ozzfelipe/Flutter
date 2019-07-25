770948cace4172ca5b6a58d95a338cedce33d292
Felipe Cardoso dos Santos <felipe.santos@amcom.com.br>
2019-07-25 12:14:30 -0300

    Finalizado app JokenPo

--- joken_po/lib/jogo.dart
+++ joken_po/lib/jogo.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class jogo extends StatefulWidget {
@@ -6,6 +8,9 @@ class jogo extends StatefulWidget {
 }
 
 class _jogoState extends State<jogo> {
+  var _imageApp = AssetImage('images/padrao.png');
+  var _resultado = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,10 +19,10 @@ class _jogoState extends State<jogo> {
         backgroundColor: Colors.blue,
       ),
       body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
           Padding(
-            padding: EdgeInsets.only(top:32,bottom: 16),
+            padding: EdgeInsets.only(top: 32, bottom: 16),
             child: Text(
               'Escolha do app:',
               textAlign: TextAlign.center,
@@ -27,9 +32,95 @@ class _jogoState extends State<jogo> {
               ),
             ),
           ),
-          Image.asset('padrao.png')
+          Image(image: this._imageApp),
+          Padding(
+            padding: EdgeInsets.only(top: 32, bottom: 16),
+            child: Text(
+              'Escolha uma opção abaixo:',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              GestureDetector(
+                onTap: () => opcaoSelecionada('papel'),
+                child: Image.asset(
+                  'images/papel.png',
+                  height: 100,
+                ),
+              ),
+              GestureDetector(
+                onTap: () => opcaoSelecionada('pedra'),
+                child: Image.asset('images/pedra.png', height: 100),
+              ),
+              GestureDetector(
+                onTap: () => opcaoSelecionada('tesoura'),
+                child: Image.asset('images/tesoura.png', height: 100),
+              ),
+            ],
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 60, bottom: 16),
+            child: Text(
+              _resultado,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
         ],
       ),
     );
+  }
+
+  void opcaoSelecionada(String escolhaUsuario) {
+    var opcoes = ['pedra', 'papel', 'tesoura'];
+
+    var numero = Random().nextInt(opcoes.length);
+    var escolhaApp = opcoes[numero];
+    alterarImageApp(escolhaApp);
+
+    resultado(escolhaApp, escolhaUsuario);
+  }
+
+  void alterarImageApp(String escolhaApp) {
+    setState(() {
+      switch (escolhaApp) {
+        case 'pedra':
+          _imageApp = AssetImage('images/pedra.png');
+          break;
+        case 'papel':
+          _imageApp = AssetImage('images/papel.png');
+          break;
+        case 'tesoura':
+          _imageApp = AssetImage('images/tesoura.png');
+          break;
+      }
+    });
+  }
+
+  void resultado(escolhaApp, escolhaUsuario) {
+    if ((escolhaApp == 'pedra' && escolhaUsuario == 'tesoura') ||
+        (escolhaApp == 'papel' && escolhaUsuario == 'pedra') ||
+        (escolhaApp == 'tesoura' && escolhaUsuario == 'papel')) {
+      setState(() {
+        _resultado = 'Você perdeu!';
+      });
+    } else if (escolhaApp == escolhaUsuario ) {
+      setState(() {
+        _resultado = 'Empate!';
+      });
+    } else {
+      setState(() {
+        _resultado = 'Você venceu!';
+      });
+    }
   }
 }
